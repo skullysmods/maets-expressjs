3c485c432a2314d73de8d167964bdaaf6cf0277a
Skully <117316808+skullysmods@users.noreply.github.com>
2025-10-24 17:33:14 +0200

    add other services & jest config

--- src/controllers/libraryController.js
+++ src/controllers/libraryController.js
@@ -1,46 +1,70 @@
-import { User, Game } from '../models/index.js';
+import { addGameToUser, getUserById, getUserGamesById, isUserHasGame, removeGameToUser } from "../services/libraryService.js";
+import { getGameById, getGameByName } from "../services/gameService.js";
 
 export const getUserGames = async (req, res) => {
-    const me = await User.findByPk(req.user.id, { include: Game });
-    res.json({
-        msg: 'Your games list.', you: {
-            id: me.id, email: me.email, games: me.Games.sort((a, b) => a.id - b.id).map(game => ({
-                id: game.id,
-                name: game.name
-            }))
-        }
-    });
+    try {
+        const user = await getUserGamesById(req.user.id);
+        res.status(200).json({
+            msg: "Your games list.",
+            you: {
+                id: user.id,
+                email: user.email,
+                games: user.Games.sort((a, b) => a.id - b.id).map((game) => ({
+                    id: game.id,
+                    name: game.name,
+                })),
+            },
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message || 'Internal server error' });
+    }
 };
 
 //TODO: support multiple game
 export const addGameToLib = async (req, res) => {
     try {
-        const user = await User.findByPk(req.params.userId);
-        const game = req.body.name;
-        console.log(game);
+        const user = await getUserById(req.params.id);
+        const game = await getGameByName(req.body.name);
 
-        if (!user || !game) return res.status(404).json({ error: 'User or Game Not found' });
+        if (!user || !game) throw new Error('User or Game Not Found');
+        if (await isUserHasGame(user, game)) throw new Error ('User already own this game')
 
-        if (await user.hasGame(game)) return res.status(409).json({ error: 'User already own this game' });
-        await user.addGame(game);
+        await addGameToUser(user, game);
 
         res.status(201).json({
-            userId: user.id, game: {
+            userId: user.id,
+            game: {
                 id: game.id,
-                name: game.name
-            }
+                name: game.name,
+            },
         });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'lib_add_failed' });
+    } catch (error) {
+        if (error.message === 'User or Game Not Found') {
+            res.status(404).json({ error: error.message });
+        } else if (error.message === 'User already own this game') {
+            res.status(409).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: "Internal server error" });
+        }
     }
 };
 
+//TODO: only for admin or self
 export const removeGameToLib = async (req, res) => {
-    const user = await User.findByPk(req.user.id);
-    const game = await Game.findByPk(req.params.id);
-    if (!game || !user) return res.status(404).json({ error: 'Not found' });
-    if (!(await user.hasGame(game))) return res.status(404).json({ error: 'User does not has this game.' });
-    await user.removeGame(game);
-    res.status(204).end();
+    try {
+        const user = await getUserById(req.params.userId);
+        const game = await getGameById(req.params.gameId);
+        if (!user || !game) throw new Error('User or Game Not Found');
+        if (!(await isUserHasGame(user, game))) throw new Error('User does not has this game.');
+        await removeGameToUser(user, game);
+        res.status(204).end();
+    } catch (error) {
+        if (error.message === 'User or Game Not Found') {
+            res.status(404).json({ error: error.message });
+        } else if (error.message === 'User does not has this game.') {
+            res.status(404).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: "Internal server error" });
+        }
+    }
 };
---
+++ src/services/libraryService.js
@@ -0,0 +1,21 @@
+import { User, Game } from '../models/index.js'
+
+export const getUserById = async (id) => {
+    return User.findByPk(id);
+}
+
+export const getUserGamesById = async (id) => {
+    return User.findByPk(id, { include: Game });
+}
+
+export const addGameToUser = async (user, game) => {
+    return user.addGame(game);
+}
+
+export const isUserHasGame = async (user, game) => {
+    return user.hasGame(game);
+}
+
+export const removeGameToUser = async (user, game) => {
+    return user.removeGame(game);
+}
